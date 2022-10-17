6839c4d080a875260a8cde0229e9201fc0f2d460
Aaron Erb <erbnlegend@gmail.com>
2022-10-17 10:40:20 -0500

    Change data structure to allow different input

---
+++ src/data.js
@@ -0,0 +1,74 @@
+const padNumbers = [
+    {
+        "keyData": 103,
+        "key": 7
+    },
+    {
+        "keyData": 104,
+        "key": 8
+    },
+    {
+        "keyData": 105,
+        "key": 9
+    },
+    {
+        "keyData": 100,
+        "key": 4
+    },
+    {
+        "keyData": 101,
+        "key": 5
+    },
+    {
+        "keyData": 102,
+        "key": 6
+    },
+    {
+        "keyData": 97,
+        "key": 1
+    },
+    {
+        "keyData": 98,
+        "key": 2
+    },
+    {
+        "keyData": 99,
+        "key": 3
+    },
+    {
+        "keyData": 96,
+        "key": 0
+    },
+    {
+        "keyData": 110,
+        "key": '.'
+    },
+    {
+        "keyData": 54,
+        "key": '^'
+    }
+];
+const padOperators = [
+    {
+        "keyData": 111,
+        "key": '/'
+    },
+    {
+        "keyData": 106,
+        "key": '*'
+    },
+    {
+        "keyData": 107,
+        "key": '+'
+    },
+    {
+        "keyData": 109,
+        "key": '-'
+    },
+    {
+        "keyData": 13,
+        "key": '='
+    }
+]
+
+export {padNumbers, padOperators}
--- src/script.js
+++ src/script.js
@@ -1,33 +1,80 @@
-const padNumbers = [7,8,9,4,5,6,1,2,3,".",0,"^"];
-const padOperators = ["/","x","+","-","="];
+import {padNumbers, padOperators} from './data.js';
 const numberPad = document.getElementById('numberPad');
 const operatorPad = document.getElementById('operatorPad');
+const screenText = document.getElementById('screenText');
 
 // Build buttons
 const buildPadNumbers = () => {
-    for(let i = 0; i < padNumbers.length; i++) {
-    const createDiv = document.createElement('div');
-    createDiv.setAttribute('class', 'button');
-    createDiv.setAttribute('id', padNumbers[i]);
-    createDiv.textContent = padNumbers[i];
-    numberPad.appendChild(createDiv);
+    for(const prop in padNumbers) {
+        const createDiv = document.createElement('div');
+        if(Object.hasOwn(padNumbers, prop)) {
+            createDiv.setAttribute('class', 'button');
+            createDiv.setAttribute('id', padNumbers[prop].key);
+            createDiv.setAttribute('keydata', padNumbers[prop].keyData)
+            createDiv.textContent = `${padNumbers[prop].key}`;
+            numberPad.append(createDiv);
+        }
     }
 }
-buildPadNumbers();
+buildPadNumbers()
 
 const buildPadOperators = () => {
-    for(let i = 0; i < padOperators.length; i++) {
-    const createDiv = document.createElement('div');
-    createDiv.setAttribute('class', 'button');
-    createDiv.setAttribute('id', padOperators[i]);
-    createDiv.textContent = padOperators[i]
-    operatorPad.appendChild(createDiv);
+    for(const prop in padOperators) {
+        const createDiv = document.createElement('div');
+        if(Object.hasOwn(padOperators, prop)) {
+            createDiv.setAttribute('class', 'button');
+            createDiv.setAttribute('id', padOperators[prop].key);
+            createDiv.setAttribute('keydata', padOperators[prop].keyData)
+            createDiv.textContent = `${padOperators[prop].key}`;
+            operatorPad.append(createDiv);
+        }
     }
 }
-buildPadOperators();
+buildPadOperators()
+
 
 // Events 
-const events = () => {
 
+// Get numberPad ids by mapping out the children
+const numberChildren = Array.from(numberPad.children);
+const getNumberIds = numberChildren.map(element => {
+    return element.id
+})
+// Get operatorPad ids by mapping out the children
+const operatorChildren = Array.from(operatorPad.children);
+const getOperatorIds = operatorChildren.map(element => {
+    return element.id
+})
+
+// Get key Event finds key push and adds to number List
+const getKey = (e) => {
+    const key = e.target.textContent;
+    keyArray.push(key);
+    characters = keyArray.join('')
+    screenText.textContent = characters;
 }
-events();
+let keyArray = [];
+let characters = ""
+const getKeyDown = (e) => {
+    const key = document.querySelector(`div[keydata="${e.keyCode}"]`);
+    const storeKey = key.textContent;
+    keyArray.push(storeKey);
+    characters = keyArray.join('')
+    screenText.textContent = characters;
+}
+// Iterate through Ids to add Events
+const createNumberEvents = (e) => {
+    for(let i = 0; i < getNumberIds.length; i ++) {
+        const addEvents = document.getElementById(getNumberIds[i]);
+        addEvents.addEventListener('click', getKey);
+    }
+}
+const createOperatorEvents = (e) => {
+    for(let i = 0; i < getOperatorIds.length; i ++) {
+        const addEvents = document.getElementById(getOperatorIds[i]);
+        addEvents.addEventListener('click', getKey);
+    }
+}
+createNumberEvents()
+createOperatorEvents()
+window.addEventListener('keydown', getKeyDown);
